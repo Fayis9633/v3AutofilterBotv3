4f001b5dd031307aac59c46237dc1e2886fdbe1d
Fayis9633 <97590049+Fayis9633@users.noreply.github.com>
2022-03-25 19:26:32 +0530

    Update inline.py

--- LuciferMoringstar_Robot/modules/inline.py
+++ LuciferMoringstar_Robot/modules/inline.py
@@ -78,8 +78,8 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('🔥 GROUP 🔥', url='https://t.me/cwfffhe'),
+        InlineKeyboardButton('🔥 CHANNEL 🔥 ', url='https://t.me/+hcFeLTutE1w4Nzc1')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
